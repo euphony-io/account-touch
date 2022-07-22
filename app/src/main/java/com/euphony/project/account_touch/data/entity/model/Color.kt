6eeaf2e4c07dda648fe0bcb37bf8e3110580d6e3
HongEunbeen <vvvv980@gmail.com>
2022-07-22 19:42:07 +0900

    [#15] chore : 유저 icon 추가
    (cherry picked from commit 976e267df9416175a13b0f74b70e178d6e742aaf)

---
+++ app/src/main/java/com/euphony/project/account_touch/data/entity/model/Color.kt
@@ -0,0 +1,14 @@
+package com.euphony.project.account_touch.data.entity.model
+
+enum class Color(val backGround: Int, val font: Int) {
+    BLUE(0x93D6DE, 0x333B58),
+    INDIGO(0x5386A1, 0xFFFFFF),
+    ORANGE(0x5386A1, 0xFFFFFF),
+    APRICOT(0x5386A1, 0x333B58),
+    GREEN(0x5386A1, 0xFFFFFF),
+    PINK(0x5386A1, 0xFFFFFF),
+    BASIE(0x5386A1, 0x333B58),
+    TURQUOISE(0x5386A1, 0xFFFFFF),
+    SKY(0x5386A1, 0x333B58),
+    BLACK(0x00FF00, 0xFFFFFF)
+}
