3ca594ee03a494a34b6632f50644aa4d8e873106
KLDAJI <kldaji@naver.com>
2022-07-22 19:22:16 +0900

    test: check isListening livedata toggled well

--- app/src/test/java/com/euphony/project/account_touch/EuphonyTestViewModelTest.kt
+++ app/src/test/java/com/euphony/project/account_touch/EuphonyTestViewModelTest.kt
@@ -2,7 +2,7 @@ package com.euphony.project.account_touch
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -58,4 +58,46 @@ class EuphonyTestViewModelTest {
             viewModel.isSpeaking.removeObserver(observer)
         }
     }
+
+    @Test
+    fun listen_setIsListeningTrue() {
+        // Given
+        val observer = Observer<Boolean> {}
+        val euTxManager = mock(EuphonyManager.getEuTxManager()::class.java)
+        val euRxManager = mock(EuphonyManager.getEuRxManager()::class.java)
+
+        try {
+            viewModel.isListening.observeForever(observer)
+
+            // When
+            viewModel.listen(euTxManager, euRxManager)
+
+            // Then
+            assertEquals(viewModel.isListening.value, true)
+        } finally {
+            viewModel.isListening.removeObserver(observer)
+        }
+    }
+
+    @Test
+    fun listen_setIsListeningFalse() {
+        // Given
+        val observer = Observer<Boolean> {}
+        val euTxManager = mock(EuphonyManager.getEuTxManager()::class.java)
+        val euRxManager = mock(EuphonyManager.getEuRxManager()::class.java)
+
+        try {
+            viewModel.isListening.observeForever(observer)
+
+            // When
+            viewModel.listen(euTxManager, euRxManager)
+            viewModel.listen(euTxManager, euRxManager)
+
+            // Then
+            assertEquals(viewModel.isListening.value, false)
+        } finally {
+            viewModel.isListening.removeObserver(observer)
+        }
+    }
+
 }
