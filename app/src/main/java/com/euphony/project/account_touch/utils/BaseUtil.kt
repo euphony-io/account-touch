ee8e6990aa045d78723202183415f21ee12ed1df
KLDAJI <kldaji@naver.com>
2022-07-23 13:22:00 +0900

    [#4] feat : 데이터 복호화를 위한 decrypt, decoding 함수 구현

--- app/src/main/java/com/euphony/project/account_touch/utils/BaseUtil.kt
+++ app/src/main/java/com/euphony/project/account_touch/utils/BaseUtil.kt
@@ -5,4 +5,5 @@ import java.util.*
 
 object BaseUtil {
     fun base64Encoding(encrypted: ByteArray): ByteArray? = Base64.getEncoder().encode(encrypted)
+    fun base64Decoding(encodedString: String) = Base64.getDecoder().decode(encodedString)
 }
--- app/src/main/java/com/euphony/project/account_touch/utils/EncryptionUtil.kt
+++ app/src/main/java/com/euphony/project/account_touch/utils/EncryptionUtil.kt
@@ -21,4 +21,20 @@ object EncryptionUtil {
         }
         return null
     }
+
+    fun decrypt(data: ByteArray, key: ByteArray, ivs: ByteArray): ByteArray? {
+        try {
+            val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+            val secretKeySpec = SecretKeySpec(key, "AES")
+            val finalIvs = ByteArray(16)
+            val len = if (ivs.size > 16) 16 else ivs.size
+            System.arraycopy(ivs, 0, finalIvs, 0, len)
+            val ivParamSpec = IvParameterSpec(finalIvs)
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParamSpec)
+            return cipher.doFinal(data)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
 }
