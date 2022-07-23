5814b1a363d5abc595d7735890be3a5e58fa754f
HongEunbeen <vvvv980@gmail.com>
2022-07-23 18:27:54 +0900

    [#17] chore : 이미지 경로 변경
    (cherry picked from commit 2f46e28710b502977f647f2deb23ecd8638cb902)

--- app/src/main/java/com/euphony/project/account_touch/data/entity/model/BankIcon.kt
+++ app/src/main/java/com/euphony/project/account_touch/data/entity/model/BankIcon.kt
@@ -5,7 +5,7 @@ enum class BankInfo(val path: String) {
     CITI("banks/citi_bank.png"),
     DGB("banks/dgb_bank.png"),
     IBK("banks/ibk_bank.png"),
-    JAJU("banks/jaju_bank.png"),
+    JEJU("banks/jaju_bank.png"),
     KAKAOBANK("banks/kakaobank_bank.png"),
     KAKAOPAY( "banks/kakaopay_bank.png"),
     KBANK("banks/kbank_bank.png"),
@@ -14,11 +14,10 @@ enum class BankInfo(val path: String) {
     KJ("banks/kj_bank.png"),
     KEB("banks/keb_bank.png"),
     MG("banks/mg_bank.png"),
-    NK("banks/nk_bank.png"),
+    NH("banks/nh_bank.png"),
     SC("banks/sc_bank.png"),
     SH("banks/sh_bank.png"),
     SINHAN("banks/sinhan_bank.png"),
     SINHYEOP( "banks/sinhyeop_bank.png"),
-    SUHYEP("banks/suhyep_bank.png"),
     WOORI( "banks/woori_bank.png")
 }
