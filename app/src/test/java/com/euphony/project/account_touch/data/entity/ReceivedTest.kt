a4cdce879003a3ef0289907416fc6e2a9634c42e
HongEunbeen <vvvv980@gmail.com>
2022-07-23 18:49:22 +0900

    [#17] test : received 객체 speaker 관련 필드 추가 테스트
    (cherry picked from commit 4b13b98b1542e851a962d43a3547570bd547554e)

--- app/src/test/java/com/euphony/project/account_touch/data/entity/ReceivedTest.kt
+++ app/src/test/java/com/euphony/project/account_touch/data/entity/ReceivedTest.kt
@@ -1,5 +1,6 @@
 package com.euphony.project.account_touch.data.entity
 
+import com.euphony.project.account_touch.data.entity.model.UserIcon
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
@@ -9,25 +10,32 @@ class ReceivedTest {
     fun 받은계좌_객체를_생성한다(){
         //given
         val accountNickname = "은빈이의 국민은행 계좌"
-        val accountNumber = 123123123123
+        val accountNumber = "123123123123"
+
+        val speakerNickname = "은빈"
+        val speakerIcon = UserIcon.GHOST
 
         //then
-        val received = Received(1L, accountNickname, accountNumber)
+        val received = Received(1L, accountNickname, accountNumber, speakerNickname, speakerIcon)
 
         //then
         assertThat(received.accountNickname).isEqualTo(accountNickname)
         assertThat(received.accountNumber).isEqualTo(accountNumber)
+        assertThat(received.speakerNickName).isEqualTo(speakerNickname)
+        assertThat(received.speakerIcon).isEqualTo(speakerIcon)
     }
 
     @Test
     fun 받은계좌_객체를_상세_조회한다(){
         //given
-        val findReceived = Received(3L, "은빈이의 국민은행 계좌", 123123123)
+        val findReceived = Received(3L, "은빈이의 국민은행 계좌", "123123123","은빈", UserIcon.GHOST)
+
         val receivedList = listOf<Received>(
-            Received(1L, "은빈이의 하나은행 계좌", 2132112321),
-            Received(2L, "은빈이의 신한은행 계좌", 15651232458),
+            Received(1L, "은빈이의 하나은행 계좌", "2132112321","은빈", UserIcon.GHOST),
+            Received(2L, "은빈이의 신한은행 계좌", "15651232458","은빈", UserIcon.GHOST),
             findReceived
         )
+
         //then
         assertThat(receivedList.contains(findReceived)).isTrue()
     }
