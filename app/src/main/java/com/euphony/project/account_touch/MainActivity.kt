4d1bbaace34a0f19786b9f4ee3532d55e6a61160
KLDAJI <kldaji@naver.com>
2022-07-26 01:04:32 +0900

    [#25] feat : AccountInfoTitle Composable 생성 (cherry-pick from 834ce4c, 965c9bb)

--- app/src/main/java/com/euphony/project/account_touch/MainActivity.kt
+++ app/src/main/java/com/euphony/project/account_touch/MainActivity.kt
@@ -5,21 +5,41 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.euphony.project.account_touch.ui.theme.AccounttouchTheme
+import com.euphony.project.account_touch.ui.theme.Blue_6D95FF
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -39,10 +59,14 @@ fun ModalBottomSheet() {
     val modalBottomSheetState =
         rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
     val coroutineScope = rememberCoroutineScope()
+    var isEditClicked by remember { mutableStateOf(false) }
 
     ModalBottomSheetLayout(
+
         sheetContent = {
-            Text("OPENED!!")
+            AccountInfoTitle(modalBottomSheetState, coroutineScope, isEditClicked) {
+                isEditClicked = !isEditClicked
+            }
         },
         sheetState = modalBottomSheetState,
         sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
@@ -53,13 +77,81 @@ fun ModalBottomSheet() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(onClick = { 
-                coroutineScope.launch { 
+            Button(onClick = {
+                isEditClicked = false
+                coroutineScope.launch {
                     modalBottomSheetState.show()
                 }
             }) {
                 Text(text = "OPEN BOTTOM SHEET")
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AccountInfoTitle(
+    modalBottomSheetState: ModalBottomSheetState,
+    coroutineScope: CoroutineScope,
+    isEditClicked: Boolean,
+    onEditClick: () -> Unit,
+) {
+    var textFieldValue by remember { mutableStateOf(TextFieldValue("부산은행")) }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (!isEditClicked) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.padding(start = 16.dp),
+                    text = textFieldValue.text,
+                    fontSize = 16.sp,
+                    color = Blue_6D95FF,
+                    fontWeight = FontWeight.Bold
+                )
+                IconButton(
+                    onClick = { onEditClick() }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Edit,
+                        contentDescription = "수정",
+                        tint = Blue_6D95FF
+                    )
+                }
+            }
+        } else {
+            TextField(
+                modifier = Modifier.padding(start = 16.dp),
+                shape = RoundedCornerShape(16.dp),
+                value = textFieldValue,
+                onValueChange = {
+                    if (it.text.length <= 10) textFieldValue = it
+                },
+                colors = TextFieldDefaults.textFieldColors(focusedIndicatorColor = Color.Transparent),
+                maxLines = 1,
+            )
+        }
+        IconButton(
+            onClick = {
+                coroutineScope.launch {
+                    modalBottomSheetState.hide()
+                }
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = "닫기",
+                tint = Blue_6D95FF
+            )
         }
     }
 }
