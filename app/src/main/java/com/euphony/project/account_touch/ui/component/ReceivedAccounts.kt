dcbbd4fc91f74c46a016103f10761ac5d45241e9
KLDAJI <kldaji@naver.com>
2022-07-26 23:25:30 +0900

    [#37] feat : ReceivedAccountItems, ReceivedAccountItem Composable 생성

--- app/src/main/java/com/euphony/project/account_touch/ui/component/ReceivedAccounts.kt
+++ app/src/main/java/com/euphony/project/account_touch/ui/component/ReceivedAccounts.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -17,26 +22,25 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.euphony.project.account_touch.data.entity.Received
 import com.euphony.project.account_touch.data.entity.User
 import com.euphony.project.account_touch.data.entity.model.UserIcon
 import com.euphony.project.account_touch.ui.theme.Blue_6D95FF
+import com.euphony.project.account_touch.ui.theme.Blue_DFE8FF
+import com.euphony.project.account_touch.ui.theme.Gray_9C9C9C
+import com.euphony.project.account_touch.ui.theme.Gray_F4F4F4
 import com.euphony.project.account_touch.utils.AssetsUtil
+import java.text.SimpleDateFormat
 
 @Composable
 fun ReceivedAccountsScreen() { // TODO: viewModel, onBackClick as parameters
@@ -45,11 +49,31 @@ fun ReceivedAccountsScreen() { // TODO: viewModel, onBackClick as parameters
         nickname = "영욱",
         icon = UserIcon.CRYING,
     )
-    ReceivedAccounts(user)
+    val receiveds = listOf<Received>(
+        Received(
+            accountNickname = "붕어빵",
+            accountNumber = "123456789",
+            speakerNickName = "붕어빵 사장",
+            speakerIcon = UserIcon.GHOST,
+        ),
+        Received(
+            accountNickname = "포장마차",
+            accountNumber = "123456789",
+            speakerNickName = "포장마차 사장",
+            speakerIcon = UserIcon.HAPPY,
+        ),
+        Received(
+            accountNickname = "옷가게",
+            accountNumber = "123456789",
+            speakerNickName = "옷가게 사장",
+            speakerIcon = UserIcon.STAR,
+        )
+    )
+    ReceivedAccounts(user, receiveds)
 }
 
 @Composable
-fun ReceivedAccounts(user: User) {
+fun ReceivedAccounts(user: User, receiveds: List<Received>) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -67,15 +91,18 @@ fun ReceivedAccounts(user: User) {
                 },
                 elevation = 0.dp
             )
+        },
+        content = {
+            Column {
+                ReceivedAccountsUser(user)
+                ReceivedAccountItems(receiveds)
+            }
         }
-    ) {
-        ReceivedAccountsUser(user)
-    }
+    )
 }
 
 @Composable
 fun ReceivedAccountsUser(user: User) {
-    var size by remember { mutableStateOf(IntSize.Zero) }
     val imageBitmap = AssetsUtil.getBitmap(LocalContext.current, user.icon.path)
 
     Row(
@@ -85,38 +112,103 @@ fun ReceivedAccountsUser(user: User) {
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
-            modifier = Modifier
-                .onSizeChanged { size = it },
             text = "${user.nickname}님께서\n받으신 계좌입니다.",
             color = Blue_6D95FF,
             fontWeight = FontWeight.Bold,
             fontSize = 22.sp
         )
-        println(size.height)
-        Box(
-            modifier = Modifier
-                .then(
-                    with(LocalDensity.current) {
-                        Modifier.size(
-                            width = size.height.toDp(),
-                            height = size.height.toDp()
-                        )
-                    }
-                )
-                .clip(RoundedCornerShape(8.dp))
-                .background(color = Blue_6D95FF),
-            contentAlignment = Alignment.Center
+        UserIconItem(imageBitmap, Blue_6D95FF)
+    }
+}
+
+@Composable
+fun UserIconItem(imageBitmap: ImageBitmap?, color: Color) {
+    Box(
+        modifier = Modifier
+            .size(60.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(color = color),
+        contentAlignment = Alignment.Center
+    ) {
+        if (imageBitmap != null) {
+            Image(
+                modifier = Modifier
+                    .size(40.dp),
+                bitmap = imageBitmap,
+                contentDescription = "유저 아이콘"
+            )
+        } else {
+            Image(
+                imageVector = Icons.Filled.Face,
+                contentDescription = "유저 아이콘 없음"
+            )
+        }
+    }
+}
+
+@Composable
+fun ReceivedAccountItems(receiveds: List<Received>) {
+    LazyColumn {
+        items(receiveds.size) {
+            ReceivedAccountItem(receiveds[it])
+        }
+    }
+}
+
+@Composable
+fun ReceivedAccountItem(received: Received) {
+    val imageBitmap = AssetsUtil.getBitmap(LocalContext.current, received.speakerIcon.path)
+    val yyyyMMdd = SimpleDateFormat("yyyy.MM.dd").format(received.createDate)
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp, horizontal = 16.dp),
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = Gray_F4F4F4,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            if (imageBitmap != null) {
-                Image(
-                    bitmap = imageBitmap,
-                    contentDescription = "유저 아이콘"
-                )
-            } else {
-                Image(
-                    imageVector = Icons.Filled.Face,
-                    contentDescription = "유저 아이콘 없음"
-                )
+            Column(
+                modifier = Modifier
+                    .padding(8.dp),
+            ) {
+                UserIconItem(imageBitmap = imageBitmap, color = Blue_DFE8FF)
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(start = 4.dp)
+                ) {
+                    Text(
+                        text = received.speakerNickName,
+                        color = Blue_6D95FF,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = received.accountNumber,
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 12.sp
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .padding(end = 16.dp)
+                ) {
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Text(
+                        text = yyyyMMdd,
+                        color = Gray_9C9C9C,
+                        fontSize = 10.sp
+                    )
+                }
             }
         }
     }
@@ -129,5 +221,25 @@ fun ReceivedAccountsPreview() {
         nickname = "영욱",
         icon = UserIcon.CRYING,
     )
-    ReceivedAccounts(user)
+    val receiveds = listOf<Received>(
+        Received(
+            accountNickname = "붕어빵",
+            accountNumber = "123456789",
+            speakerNickName = "붕어빵 사장",
+            speakerIcon = UserIcon.GHOST,
+        ),
+        Received(
+            accountNickname = "포장마차",
+            accountNumber = "123456789",
+            speakerNickName = "포장마차 사장",
+            speakerIcon = UserIcon.HAPPY,
+        ),
+        Received(
+            accountNickname = "옷가게",
+            accountNumber = "123456789",
+            speakerNickName = "옷가게 사장",
+            speakerIcon = UserIcon.STAR,
+        )
+    )
+    ReceivedAccounts(user, receiveds)
 }
