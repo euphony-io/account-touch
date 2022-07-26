de543d14721f9ca1ded36dab76a76abd1f692271
KLDAJI <kldaji@naver.com>
2022-07-27 00:41:52 +0900

    [#43] feat : 송신자 정보 Composable 구현

--- app/src/main/java/com/euphony/project/account_touch/ui/component/ReceivedDetail.kt
+++ app/src/main/java/com/euphony/project/account_touch/ui/component/ReceivedDetail.kt
@@ -10,17 +10,27 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.euphony.project.account_touch.data.entity.Received
+import com.euphony.project.account_touch.data.entity.model.UserIcon
 import com.euphony.project.account_touch.ui.theme.Blue_6D95FF
 
 @Composable
 fun ReceivedDetailScreen() { // TODO: viewMode, onBackClick as parameters
-    ReceivedDetail()
+    val received = Received(
+        accountNickname = "붕어빵",
+        accountNumber = "123456789",
+        speakerNickName = "붕어빵 사장",
+        speakerIcon = UserIcon.GHOST,
+    )
+    ReceivedDetail(received)
 }
 
 @Composable
-fun ReceivedDetail() {
+fun ReceivedDetail(received: Received) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -38,14 +48,33 @@ fun ReceivedDetail() {
                 },
                 elevation = 0.dp
             )
+        },
+        content = {
+            SpeakerInfo(received)
         }
-    ) {
+    )
+}
 
-    }
+@Composable
+fun SpeakerInfo(received: Received) {
+    Text(
+        modifier = Modifier
+            .padding(vertical = 24.dp, horizontal = 16.dp),
+        text = "${received.speakerNickName}님꼐서 보내신\n${received.accountNickname} 계좌입니다.",
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        color = Blue_6D95FF
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ReceivedDetailPreview() {
-    ReceivedDetail()
+    val received = Received(
+        accountNickname = "붕어빵",
+        accountNumber = "123456789",
+        speakerNickName = "붕어빵 사장",
+        speakerIcon = UserIcon.GHOST,
+    )
+    ReceivedDetail(received)
 }
