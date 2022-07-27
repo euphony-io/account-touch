2fc5bac9e170f88e8f67fb9917c590c2f57b8288
HongEunbeen <vvvv980@gmail.com>
2022-07-27 20:39:42 +0900

    [#28] feat : ui단에서 entity 호출 막음 (temp data 생성

---
+++ app/src/main/java/com/euphony/project/account_touch/ui/component/TempData.kt
@@ -0,0 +1,23 @@
+package com.euphony.project.account_touch.ui.component
+
+import com.euphony.project.account_touch.utils.model.UserIcon
+import java.util.*
+
+data class User(
+    val nickname: String,
+    val icon: UserIcon,
+)
+
+data class Received(
+    val accountNickname: String,
+    val accountNumber: String,
+    val speakerNickName: String,
+    val speakerIcon: UserIcon,
+    val createDate: Date = Date(),
+    val modifyDate: Date = Date()
+)
+
+data class Bank(
+    val id: Long,
+)
+
