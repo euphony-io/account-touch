275382daf0cad286255cd714d8e7695507239881
KLDAJI <kldaji@naver.com>
2022-07-27 16:00:34 +0900

    [#47] feat : 계좌 송신 정보 Composable 생성

--- app/src/main/java/com/euphony/project/account_touch/ui/component/TransmitAccount.kt
+++ app/src/main/java/com/euphony/project/account_touch/ui/component/TransmitAccount.kt
@@ -1,6 +1,9 @@
 package com.euphony.project.account_touch.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
@@ -11,8 +14,11 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.euphony.project.account_touch.R
 import com.euphony.project.account_touch.data.entity.Account
 import com.euphony.project.account_touch.data.entity.User
@@ -58,10 +64,28 @@ fun TransmitAccount(user: User, account: Account) {
         },
         content = {
             Column {
-
+                TransmitAccountInfo(user, account)
             }
         }
     )
+}
+
+@Composable
+fun TransmitAccountInfo(user: User, account: Account) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "${user.nickname}의\n${account.nickname} 계좌를\n전송합니다.",
+            fontWeight = FontWeight.Bold,
+            fontSize = 26.sp,
+            textAlign = TextAlign.Center,
+            color = Blue_6D95FF
+        )
+    }
 }
 
 @Preview(showBackground = true)
