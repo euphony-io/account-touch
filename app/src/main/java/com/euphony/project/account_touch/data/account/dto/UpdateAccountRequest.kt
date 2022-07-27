d84f1368f69b08a0704ff766c37789272d8a2e90
HongEunbeen <vvvv980@gmail.com>
2022-07-27 19:07:19 +0900

    [#28] feat : account repository 생성
    (cherry picked from commit ece4c8190e1571d7cfa1c4bf47b59f0a369b5e9a)

---
+++ app/src/main/java/com/euphony/project/account_touch/data/account/dto/UpdateAccountRequest.kt
@@ -0,0 +1,15 @@
+package com.euphony.project.account_touch.data.account.dto
+
+import com.euphony.project.account_touch.data.global.base.request.BaseRequest
+import com.euphony.project.account_touch.utils.model.Color
+
+class UpdateAccountRequest(
+    val id: Long,
+    val color: Color,
+    val isAlwaysOn : Boolean
+): BaseRequest {
+    override fun validate(): Boolean {
+        if(id < 0) return false
+        return true
+    }
+}
---
+++ app/src/main/java/com/euphony/project/account_touch/data/account/repository/AccountRepository.kt
@@ -0,0 +1,48 @@
+package com.euphony.project.account_touch.data.account.repository
+
+import androidx.annotation.WorkerThread
+import com.euphony.project.account_touch.data.account.entity.Account
+import com.euphony.project.account_touch.data.account.dao.AccountDao
+import com.euphony.project.account_touch.data.account.dto.CreateAccountRequest
+import com.euphony.project.account_touch.data.account.dto.UpdateAccountRequest
+import com.euphony.project.account_touch.data.global.AccountWithBank
+import com.euphony.project.account_touch.data.global.base.response.Response
+import kotlinx.coroutines.flow.Flow
+
+class AccountRepository(private val dao : AccountDao){
+
+    //getAccounts
+    fun getAccounts(): Flow<List<AccountWithBank>>{
+        return dao.findAllBy()
+    }
+
+    //getAccountById
+    fun getAccount(id: Long): Flow<Account>{
+        return dao.findById(id)
+    }
+
+    //insert
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun createAt(request: CreateAccountRequest) {
+        dao.insert(request.toEntity())
+    }
+
+    //update
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun modifyAccount(request: UpdateAccountRequest) {
+        dao.update(
+            id=request.id,
+            isAlwaysOn=request.isAlwaysOn,
+            modifyColor=request.color,
+        )
+    }
+
+    //delete
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteAccount(account: Account) {
+        dao.delete(account);
+    }
+}
