7499d790e298ce4b5af699b2a1a8e719c4196f2e
HongEunbeen <vvvv980@gmail.com>
2022-07-27 19:28:37 +0900

    [#28] test : bank flow 데이터 추가로 테스트 코드 수정
    (cherry picked from commit a6a9802c3558a2938306dc2b5e1c96c9da53af67)

--- app/src/androidTest/java/com/euphony/project/account_touch/data/source/dao/BankDaoTest.kt
+++ app/src/androidTest/java/com/euphony/project/account_touch/data/source/dao/BankDaoTest.kt
@@ -3,78 +3,90 @@ package com.euphony.project.account_touch.data.source.dao
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import com.euphony.project.account_touch.data.entity.Bank
-import com.euphony.project.account_touch.data.entity.model.BankIcon
-import com.euphony.project.account_touch.data.entity.model.ExternalPackage
-import com.euphony.project.account_touch.data.source.EuphonyDatabase
+import com.euphony.project.account_touch.data.bank.dao.BankDao
+import com.euphony.project.account_touch.data.bank.entity.Bank
+import com.euphony.project.account_touch.utils.model.BankIcon
+import com.euphony.project.account_touch.utils.model.ExternalPackage
+import com.euphony.project.account_touch.data.global.config.EuphonyDatabase
 import com.google.common.truth.Truth.assertThat
 import junit.framework.TestCase
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
 @RunWith(AndroidJUnit4::class)
 class BankDaoTest  : TestCase() {
 
     private lateinit var dao: BankDao
     private lateinit var db: EuphonyDatabase
 
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testScope = TestCoroutineScope(testDispatcher)
+
     @Before
     public override fun setUp() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        db = Room.inMemoryDatabaseBuilder(
-            appContext,
-            EuphonyDatabase::class.java
-        ).build()
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        db = Room.inMemoryDatabaseBuilder(context, EuphonyDatabase::class.java)
+            .setTransactionExecutor(testDispatcher.asExecutor())
+            .setQueryExecutor(testDispatcher.asExecutor())
+            .build()
 
         dao = db.getBankDao()
     }
 
     @Test
-    fun 은행_생성_조회() = runBlocking {
+    fun 은행_생성() = testScope.runBlockingTest {
         //given
         val bankName = "국민은행"
         val bank = Bank(1L, bankName, BankIcon.KB, 12, ExternalPackage.KOOKMIN)
 
         //when
-        dao.addBank(bank)
-        val newBanks = dao.getAll()
-
+        dao.insert(bank)
         //then
-        assertThat(newBanks).isNotEmpty()
-        assertThat(newBanks[0].name).isEqualTo(bankName)
+
+        dao.findAllBy().take(1).collect { bank ->
+            assertThat(bank[0].bankIconPath).isNotNull()
+            assertThat(bank[0].name).isEqualTo(bankName)
+        }
     }
 
     @Test
-    fun 은행_리스트_생성_조회() = runBlocking {
+    fun 은행_리스트_생성_조회() = testScope.runBlockingTest {
         //given
         val banks = arrayListOf<Bank>(
             Bank(1L, "국민은행", BankIcon.KB, 12, ExternalPackage.KOOKMIN),
-            Bank(2L, "케이뱅크",BankIcon.KBANK, 12, ExternalPackage.KBANK),
+            Bank(2L, "케이뱅크", BankIcon.KBANK, 12, ExternalPackage.KBANK),
             Bank(3L, "기업은행", BankIcon.IBK, 12, ExternalPackage.IBK),
             Bank(4L, "하나은행", BankIcon.KEB, 12, ExternalPackage.KEB)
         )
+        dao.insertAll(banks)
 
         //when
-        dao.addBanks(banks)
-        val newBanks = dao.getAll()
-
-        //then
-        assertThat(newBanks.size).isEqualTo(banks.size)
-        assertThat(newBanks[0]).isEqualTo(banks[0])
+        dao.findAllBy().take(1).collect { bank ->
+            //then
+            assertThat(bank.size).isEqualTo(4)
+        }
     }
 
     @Test
-    fun 은행_상세_조회() = runBlocking {
+    fun 은행_상세_조회() = testScope.runBlockingTest {
         //given
         val bankName = "국민은행"
         val bank =  Bank(1L, bankName, BankIcon.KB, 12, ExternalPackage.KOOKMIN)
-        dao.addBank(bank)
+        dao.insert(bank)
 
         //when
-        val newBank = dao.getBankById(1L)
+        val newBank = dao.findById(1L)
 
         //then
         assertThat(newBank).isNotNull()
@@ -82,14 +94,14 @@ class BankDaoTest  : TestCase() {
     }
 
     @Test
-    fun 은행_앱패키지_조회() = runBlocking {
+    fun 은행_앱패키지_조회() = testScope.runBlockingTest {
         //given
         val ppackage = ExternalPackage.KAKAOPAY
         val bank =  Bank(1L, "카카오뱅크", BankIcon.KAKAOBANK, 12, ppackage)
-        dao.addBank(bank)
+        dao.insert(bank)
 
         //when
-        val newPackage = dao.getAppPackageById(1L)
+        val newPackage = dao.findAppPackageById(1L)
 
         //then
         assertThat(newPackage).isNotNull()
