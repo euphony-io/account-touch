9d7e93140ed4b1ce4d56369c93cd65d296da703a
유나연 <yny026@gmail.com>
2022-07-28 20:57:24 +0900

    다이얼로그 화면 구현(성공/실패 여부에 따른 string 파라미터값 전달)

--- app/src/main/java/com/euphony/project/account_touch/ui/component/Dialog.kt
+++ app/src/main/java/com/euphony/project/account_touch/ui/component/Dialog.kt
@@ -1,5 +1,7 @@
 package com.euphony.project.account_touch.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -9,23 +11,26 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.euphony.project.account_touch.R
 import com.euphony.project.account_touch.ui.screen.loadText
 import com.euphony.project.account_touch.ui.screen.space
 import com.euphony.project.account_touch.ui.theme.Blue_6D95FF
+import com.euphony.project.account_touch.ui.theme.grey
 import com.euphony.project.account_touch.ui.theme.mainColor
 import com.euphony.project.account_touch.ui.theme.white
 
 @Preview(showBackground = true)
 @Composable
 fun DialogPreview() {
-    TransmitDialog("계좌 보내기 성공")
+    TransmitDialog(str = "계좌 보내기 성공")   //or 계좌 보내기 실패
 }
 
 @Composable
@@ -35,7 +40,7 @@ fun TransmitDialog(str:String){
             modifier = Modifier
                 .width(200.dp)
                 .wrapContentHeight(),
-            shape = RoundedCornerShape(12.dp),
+            shape = RoundedCornerShape(15.dp),
             color = white
         ){
             successContent(str)
@@ -48,19 +53,24 @@ fun successContent(str:String){
     Column(
         modifier = Modifier
             .padding(vertical = 30.dp),
-        verticalArrangement =  Arrangement.Center
+        verticalArrangement =  Arrangement.Center,
+        Alignment.CenterHorizontally
     ){
         loadText("${str}")
-        space(value = 20)
+        space(value = 10)
+
         TransmitImage()
         space(20)
+
         TextRound("닉네임 님")
-        Row(Modifier.padding(end = 5.dp)){
+        space(value = 5)
+        Row(Modifier.padding(horizontal = 10.dp)){
             TextRound("송신자님")
-            TextRound(str = "수신자님")
+            TextRound("수신자님")
         }
 
         //확인 버튼
+        space(value = 20)
         Button(
             onClick = {
                       //다이얼로그 닫기 작업
@@ -85,9 +95,11 @@ fun successContent(str:String){
 fun TextRound(str:String){
     Box(
         modifier = Modifier
-            .fillMaxWidth(),
+            .clip(RoundedCornerShape(18.dp))
+            .background(grey)
+            .padding(vertical = 5.dp, horizontal = 10.dp),
         contentAlignment = Alignment.Center,
     ){
-        Text("${str}")
+        Text("${str}", color = mainColor, fontSize = 12.sp)
     }
 }
