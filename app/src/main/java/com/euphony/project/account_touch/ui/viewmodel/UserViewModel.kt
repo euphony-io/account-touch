4f2ef5449919ee6e070b111466b0d34522328ae6
HongEunbeen <vvvv980@gmail.com>
2022-07-29 20:11:11 +0900

    [#30] feat : viewModel 작성
    (cherry picked from commit dc701cc4c9dbdc143bfd4e33d7badfbf75f7f233)

---
+++ app/src/main/java/com/euphony/project/account_touch/ui/viewmodel/UserViewModel.kt
@@ -0,0 +1,30 @@
+package com.euphony.project.account_touch.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.euphony.project.account_touch.data.user.dto.CreateUserRequest
+import com.euphony.project.account_touch.data.user.entity.User
+import com.euphony.project.account_touch.data.user.repository.UserRepository
+import com.euphony.project.account_touch.utils.model.UserIcon
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UserViewModel @Inject constructor (
+    private val repository: UserRepository
+) : ViewModel() {
+
+    val user: LiveData<User> = repository.getUser(0).asLiveData()
+
+    fun addUser(request: CreateUserRequest) = viewModelScope.launch {
+        repository.createAt(request)
+    }
+
+    fun modifyUser(id: Long, nickname: String, modifyIcon: UserIcon) = viewModelScope.launch {
+        repository.updateUser(id, nickname, modifyIcon)
+    }
+
+}
