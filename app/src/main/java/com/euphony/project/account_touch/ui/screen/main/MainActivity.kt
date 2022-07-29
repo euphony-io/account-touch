4500569680b680a930b95dc1651ace955b900cac
KLDAJI <kldaji@naver.com>
2022-07-29 17:13:53 +0900

    [#59] chore : 주석 삭제

--- app/src/main/java/com/euphony/project/account_touch/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/euphony/project/account_touch/ui/screen/main/MainActivity.kt
@@ -65,21 +65,14 @@ fun MainNavHost(navController: NavHostController) {
     }
 }
 
-// navigate 중복 호출 방지 (google codelab)
 fun NavHostController.navigateSingleTopTo(route: String) =
     this.navigate(route) {
-        // Pop up to the start destination of the graph to
-        // avoid building up a large stack of destinations
-        // on the back stack as users select items
         popUpTo(
             this@navigateSingleTopTo.graph.findStartDestination().id
         ) {
             saveState = true
         }
-        // Avoid multiple copies of the same destination when
-        // reselecting the same item
         launchSingleTop = true
-        // Restore state when reselecting a previously selected item
         restoreState = true
     }
 
